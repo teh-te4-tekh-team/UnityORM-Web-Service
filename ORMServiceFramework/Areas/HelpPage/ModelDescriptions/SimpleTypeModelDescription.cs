4904076d63e1d1317826bebba93ecc58197c2d28
Simeon <s_sheitanov@abv.bg>
2016-10-26 22:53:01 +0300

    Added initial testing project

---
+++ ORMServiceFramework/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ORMServiceFramework.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ORMServiceFramework/Controllers/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using ORMServiceFramework.Models;
+
+namespace ORMServiceFramework.Controllers
+{
+    public class PlayerController : ApiController
+    {
+        private GameContext gameContext = new GameContext();
+            
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]Player value)
+        {
+            this.gameContext.Players.Add(value);
+            this.gameContext.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        public IEnumerable<Player> Get()
+        {
+            return this.gameContext.Players.Select(x => x);
+        }
+
+        public Player Get(int id)
+        {
+            return this.gameContext.Players.FirstOrDefault();
+        }
+    }
+}
