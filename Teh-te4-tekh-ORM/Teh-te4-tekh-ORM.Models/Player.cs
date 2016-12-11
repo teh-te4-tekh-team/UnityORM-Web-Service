847a9c568edb7e03bc5736a2396b5c738bb49535
Gesh4o <georgi969696@abv.bg>
2016-12-11 18:11:47 +0200

    Player models rename.

---
+++ Teh-te4-tekh-ORM/Teh-te4-tekh-ORM.Models/Player.cs
@@ -0,0 +1,19 @@
+﻿namespace Teh_te4_tekh_ORM.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Previosly known as GameUser.
+    /// </summary>
+    public class Player
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [Index(IsUnique = true)]
+        public string Username { get; set; }
+    }
+}
---
+++ Teh-te4-tekh-ORM/Teh-te4-tekh-ORM.Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Teh_te4_tekh_ORM.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Previously known as ApplicationUser.
+    /// </summary>
+    public class User
+    {
+        [Key]
+        public int User { get; set; }
+
+        [StringLength(100)]
+        [Index(IsUnique = true)]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
